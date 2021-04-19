c1af6955ab2d9c4de0b16cd985f953a78d3edc20
AlexandrSilin <67175@mail.ru>
2021-04-19 17:43:58 +0300

    11 lesson

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://i.pinimg.com/236x/26/f0/db/26f0db155bf6a2508c52040084eba161.jpg'></img>
       </header>
+      <nav className='nav'>
+        <div>
+          <a href='#s'>Profile</a>
+        </div>
+        <div>
+          <a href='#s'>Messages</a>
+        </div>
+        <div>
+          <a href='#s'>News</a>
+        </div>
+        <div>
+          <a href='#s'>Music</a>
+        </div>
+        <div>
+          <a href='#s'>Settings</a>
+        </div>
+      </nav>
+      <div className='content'>
+        <div>
+          <img src='https://s1.1zoom.ru/big0/596/Evening_Forests_Mountains_Firewatch_Campo_Santo_549147_1280x720.jpg'></img>
+        </div>
+        <div>ava + desc</div>
+        <div>
+          my posts
+          <div>
+            new post
+          </div>
+          <div>
+            post1
+          </div>
+          <div>
+            post2
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
